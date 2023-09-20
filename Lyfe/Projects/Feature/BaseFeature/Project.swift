e02210839b07fc70dcfa68463570e63726450e3b
woogi <azicplus@naver.com>
2023-09-21 01:12:04 +0900

    add: BaseFeature Interface with TCACoordinators

--- Lyfe/Projects/Feature/BaseFeature/Project.swift
+++ Lyfe/Projects/Feature/BaseFeature/Project.swift
@@ -5,7 +5,11 @@ import ProjectDescriptionHelpers
 let project = Project.module(
     name: ModulePaths.Feature.BaseFeature.rawValue,
     targets: [
+        .interface(module: .feature(.BaseFeature), dependencies: [
+            .SPM.TCACoordinators
+        ]),
         .implements(module: .feature(.BaseFeature), dependencies: [
+            .feature(target: .BaseFeature, type: .interface),
             .userInterface(target: .DesignSystem),
             .shared(target: .GlobalThirdPartyLibrary)
         ]),
