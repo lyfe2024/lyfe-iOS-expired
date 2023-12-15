fce68fc6e998a80851969e312b55ff16fc6e02b5
HenryVoid <azicplus@naver.com>
2023-12-15 15:34:36 +0900

    feat: DesignSystem
    Entity - FocusField, ViewStatus

---
+++ Lyfe/Projects/UserInterface/DesignSystem/Sources/View+Entity/FocusField.swift
@@ -0,0 +1,15 @@
+//
+//  FocusField.swift
+//  DesignSystem
+//
+//  Created by 송형욱 on 12/15/23.
+//  Copyright © 2023 com.lyfe.lyfe. All rights reserved.
+//
+
+import Foundation
+
+public enum FocusField {
+    case on
+    case off
+    case error
+}
---
+++ Lyfe/Projects/UserInterface/DesignSystem/Sources/View+Entity/ViewStatus.swift
@@ -0,0 +1,15 @@
+//
+//  ViewStatus.swift
+//  DesignSystem
+//
+//  Created by 송형욱 on 12/15/23.
+//  Copyright © 2023 com.lyfe.lyfe. All rights reserved.
+//
+
+import Foundation
+
+public enum ViewStatus {
+    case success
+    case failure
+    case `default`
+}
