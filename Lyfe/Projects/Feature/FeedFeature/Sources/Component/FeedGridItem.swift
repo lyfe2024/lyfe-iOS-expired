c4e3584899263d8d09831a9afd211144821ba006
chayoosang <yoo7969@naver.com>
2023-12-17 18:27:56 +0900

    [LFI-15] feat: 피드 Grid Item 추가 및 Feed Grid UI 구현

---
+++ Lyfe/Projects/Feature/FeedFeature/Sources/Component/FeedGridItem.swift
@@ -0,0 +1,19 @@
+//
+//  FeedGridItem.swift
+//  FeedFeatureInterface
+//
+//  Created by 유상 on 12/17/23.
+//  Copyright © 2023 com.lyfe.lyfe. All rights reserved.
+//
+
+import SwiftUI
+
+struct FeedGridItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedGridItem()
+}
