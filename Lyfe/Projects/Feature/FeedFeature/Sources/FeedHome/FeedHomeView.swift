ac34a60530f2e35553466cd9886342cdf189005f
chayoosang <yoo7969@naver.com>
2023-12-17 18:12:15 +0900

    [LFI-15] feat: 피드 상단 탭바 구현

---
+++ Lyfe/Projects/Feature/FeedFeature/Sources/FeedHome/FeedHomeView.swift
@@ -0,0 +1,19 @@
+//
+//  FeedHomeView.swift
+//  FeedFeatureInterface
+//
+//  Created by 유상 on 12/17/23.
+//  Copyright © 2023 com.lyfe.lyfe. All rights reserved.
+//
+
+import SwiftUI
+
+struct FeedHomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedHomeView()
+}
