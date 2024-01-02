cfe3a72349a04a4833c114a192967a81092fb9f7
차유상 <81149634+chayoosang@users.noreply.github.com>
2024-01-03 00:12:44 +0900

    [LFI-19] feat: 프로필 설정 로그인 시 화면 구현

--- Lyfe/Projects/Feature/ProfileFeature/Sources/Setup/SetupView.swift
+++ Lyfe/Projects/Feature/ProfileFeature/Sources/Setup/SetupView.swift
@@ -17,6 +17,9 @@ struct SetupView: View {
         self.store = store
     }
     
+    // MARK: Domain 연결 후 Core로 옮기기 - 임시 데이터
+    @State var isNotification: Bool = true
+    
     var body: some View {
         VStack(spacing: 16.0) {
             HStack {
@@ -27,11 +30,35 @@ struct SetupView: View {
             }.padding(.leading, 20)
             
             List {
-                SetupListItem(title: "이용약관")
+                VStack(alignment: .center) {
+                    HStack {
+                        Toggle(isOn: $isNotification) {
+                            Text("알림 설정")
+                                .font(.system(size: 16))
+                        }
+                        .tint(R.Color.mainOrange500)
+                    }.padding(.vertical, 12)
+                }.listRowSeparator(.hidden)
+                SetupListItem(title: "스크랩")
                     .listRowSeparator(.hidden)
-                SetupListItem(title: "개인정보처리방침")
+                SetupListItem(title: "사용경험")
+                    .listRowSeparator(.hidden)
+                SetupListItem(title: "개인정보 수집 및 이용 동의")
+                    .listRowSeparator(.hidden)
+                SetupListItem(title: "회원탈퇴")
                     .listRowSeparator(.hidden)
             }.listStyle(.plain)
+            
+            Spacer()
+            
+            
+            DefaultButton(
+                text: "로그아웃",
+                isEnabled: true,
+                tapAction: { }
+            )
+            .padding(.bottom, 24)
+            .padding(.horizontal, 20)
             
         }.topBar(
             leftView: {
